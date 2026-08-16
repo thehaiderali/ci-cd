9ef299cd38186da53bacfd67eaea2ea9d1311733
Haider Ali <hyder6291@gmail.com>
2026-08-16 21:28:35 +0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express"
+
+const app=express()
+const port=process.env.PORT || 4000
+app.use(express.json())
+
+app.get("/",(req,res)=>res.json({
+    message:"CI-CD Server API"
+}))
+
+app.get("/health",(req,res)=>res.status(200).json({
+    status:"OK",
+    timestamp:Date.now(),
+
+}))
+
+app.listen(port,()=>{
+    console.log("The Server is Running Fine")
+})
